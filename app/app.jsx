b9e4214d301570ac2ed9482a0986d73cf61172d7
Nick Hester <nickhstr@gmail.com>
2016-09-17 16:54:02 -0700

    refactor: update structure and configuration

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-	<h1>Boilerplate app!</h1>,
-	document.getElementById('app')
+  <h1>React Boilerplate App</h1>,
+  document.getElementById('app')
 );

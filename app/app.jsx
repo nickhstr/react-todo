116b18cbb8d8c6a70b8d61876859681ee85d4716
Nick Hester <nickhstr@gmail.com>
2016-09-19 15:40:24 -0700

    feat: add test suite for list component

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 // Load foundation
 $(document).foundation();
 
@@ -9,6 +11,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <h1>React Boilerplate App</h1>,
+  <TodoApp></TodoApp>,
   document.getElementById('app')
 );

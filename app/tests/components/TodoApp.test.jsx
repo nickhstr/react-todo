bc7922f7f509db1ef3725c2c83402dfa3aebf590
Nick Hester <nickhstr@gmail.com>
2016-09-19 23:14:35 -0700

    feat: add completed toggle tests

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -13,11 +13,25 @@ describe('TodoApp', () => {
 
 	it('should add todo to the todos state on handleAddTodo', () => {
 		var todoText = 'test text';
-		var todoApp = TestUtils.renderIntoDocument(<TodoApp ></TodoApp>);
+		var todoApp = TestUtils.renderIntoDocument(<TodoApp></TodoApp>);
 
 		todoApp.setState({todos: []});
 		todoApp.handleAddTodo(todoText);
 
 		expect(todoApp.state.todos[0].text).toBe(todoText);
+	});
+
+	it('should toggle completed value when handleToggle called', () => {
+		var todoData = {
+			id: 11,
+			text: 'Test features',
+			completed: false
+		};
+		var todoApp = TestUtils.renderIntoDocument(<TodoApp></TodoApp>);
+		todoApp.setState({todos: [todoData]});
+
+		expect(todoApp.state.todos[0].completed).toBe(false);
+		todoApp.handleToggle(11);
+		expect(todoApp.state.todos[0].completed).toBe(true);
 	});
 });

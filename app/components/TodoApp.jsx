3abcf6031364621cefe9da213aeb00b8e9a51a6c
Nick Hester <nickhstr@gmail.com>
2016-09-19 17:08:32 -0700

    feat: add TodoSearch

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,13 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
 	getInitialState: function() {
 		return {
+			showCompleted: false,
+			searchText: '',
 			todos: [
 				{
 					id: 1,
@@ -25,6 +28,12 @@ var TodoApp = React.createClass({
 			]
 		}
 	},
+	handleSearch: function(showCompleted, searchText) {
+		this.setState({
+			showCompleted: showCompleted,
+			searchText: searchText.toLowerCase()
+		});
+	},
 	handleAddTodo: function(text) {
 		alert('new todo: ' + text);
 	},
@@ -32,6 +41,7 @@ var TodoApp = React.createClass({
 		var {todos} = this.state;
 		return (
 			<div>
+				<TodoSearch onSearch={this.handleSearch}></TodoSearch>
 				<TodoList todos={todos}></TodoList>
 				<AddTodo onAddTodo={this.handleAddTodo}></AddTodo>
 			</div>

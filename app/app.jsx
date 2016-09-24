b8282bdbb4333861e225f5dd47f321c27e04b05e
Nick Hester <nickhstr@gmail.com>
2016-09-24 16:08:37 -0700

    refactor: reconnect to localStorage

--- app/app.jsx
+++ app/app.jsx
@@ -7,10 +7,15 @@ var TodoApp = require('TodoApp');
 
 var actions = require('actions');
 var store = require('configureStore').configure();
+var TodoAPI = require('TodoAPI');
 
 store.subscribe(() => {
-	console.log('New state', store.getState());
+	var state = store.getState();
+	TodoAPI.setTodos(state.todos);
 });
+
+var initialTodos = TodoAPI.getTodos();
+store.dispatch(actions.addTodos(initialTodos));
 
 // Load foundation
 $(document).foundation();
